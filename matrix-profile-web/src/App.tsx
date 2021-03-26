f0785d69d1076015e038845807059b97f64c6e5a
Renato <renato145@hotmail.com>
2021-03-26 22:19:10 +1030

    add zustand store

--- matrix-profile-web/src/App.tsx
+++ matrix-profile-web/src/App.tsx
@@ -2,13 +2,15 @@ import React from "react";
 
 export const App = () => {
   return (
-    <div className="container p-2">
+    <div className="container px-10 py-4">
       <p className="text-5xl font-bold">Matrix Profile</p>
-      <p>upload data button</p>
-      <p>info for uploading</p>
-      <p>load sample data button</p>
-      <p>plot line</p>
-      <p>perform calculation</p>
+      <div className="mt-6">
+        <p>upload data button</p>
+        <p>info for uploading</p>
+        <p>load sample data button</p>
+        <p>plot line</p>
+        <p>perform calculation</p>
+      </div>
     </div>
   );
 };
---
+++ matrix-profile-web/src/store.ts
@@ -0,0 +1,6 @@
+import create from "zustand";
+
+export type TStore = {
+}
+
+export const useStore = create<TStore>(() => ({}));
