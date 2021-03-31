9a5d93f78561f431f310d1dd205f46f8c1e23333
Renato <renato145@hotmail.com>
2021-03-31 22:57:01 +1030

    css responsive

--- matrix-profile-web/src/App.tsx
+++ matrix-profile-web/src/App.tsx
@@ -4,16 +4,16 @@ import { Plot } from "./components/Plot";
 import { DataDescription } from "./components/DataDescription";
 import { CalculateBtn } from "./components/CalculateBtn";
 import { WindowSizeSel } from "./components/WindowSizeSel";
+import { UploadBtn } from "./components/UploadBtn";
 
 export const App = () => {
   return (
     <div className="flex flex-col container mx-auto px-10 pt-6 min-h-screen">
       <p className="text-5xl font-bold">Matrix Profile</p>
-      <div className="mt-8 pr-4 flex justify-between">
-        {/* <p>upload data button</p> */}
-        {/* <p>info for uploading</p> */}
+      <div className="mt-8 pr-4 flex flex-col sm:flex-row flex-wrap justify-between">
         <WindowSizeSel />
-        <div className="flex space-x-2">
+        <div className="mt-2 sm:mt-0 flex space-x-2">
+          <UploadBtn />
           <LoadSampleBtn />
           <CalculateBtn />
         </div>
