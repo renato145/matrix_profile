71a4cc8326411b1708ebc12274dd7ecb08f4e945
Renato <renato145@hotmail.com>
2021-04-02 00:01:50 +1030

    responsive styles

--- matrix-profile-web/src/App.tsx
+++ matrix-profile-web/src/App.tsx
@@ -9,12 +9,12 @@ import { RepoDescription } from "./components/RepoDescription";
 
 export const App = () => {
   return (
-    <div className="flex flex-col container mx-auto px-10 pt-6 pb-2 min-h-screen">
+    <div className="flex flex-col container mx-auto px-4 md:px-10 pt-6 pb-2 min-h-screen">
       <p className="text-5xl font-bold">Matrix Profile</p>
-      <RepoDescription />
-      <div className="mt-8 pr-4 flex flex-col sm:flex-row flex-wrap justify-between">
+      <RepoDescription className="mt-4 sm:mt-8" />
+      <div className="mt-4 sm:mt-8 pr-4 flex flex-col sm:flex-row flex-wrap justify-between">
         <WindowSizeSel />
-        <div className="mt-2 sm:mt-0 flex space-x-2">
+        <div className="mt-3 sm:mt-0 flex space-x-2">
           <UploadBtn />
           <LoadSampleBtn />
           <CalculateBtn />
